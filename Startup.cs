acad2732630a1c5ba1ce7ffe702d7a1fefc996d9
Liword38 <matnico@laposte.net>
2019-11-20 02:19:04 +0100

    Routage entre les pages + système d'authentification + BDD Users (pas complète)

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ASP_Test.Startup))]
+namespace ASP_Test
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
